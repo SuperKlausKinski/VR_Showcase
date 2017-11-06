379a64dc83ebf022498b9448ed14332946d96901
Pepe <peerdraeger@googlemail.com>
2017-11-06 02:00:40 +0100

    + audio loops

---
+++ Assets/MediaStation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GVR;
+
+namespace VRControlls.Demo
+{
+    public class MediaStation : MonoBehaviour
+    {
+        public GvrAudioSource AudioSourceLeft;
+        public GvrAudioSource AudioSourceRight;
+        public Collider[] Collider;
+        //-------------------------------------------------------------------------------------------------------------
+        void Awake()
+        {
+            ToggleStationInteractability(false);
+            PlayAudio(false);
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void PlayAudio(bool _active)
+        {
+            if (_active)
+            {
+                AudioSourceLeft.Play();
+                AudioSourceRight.Play();
+            }
+            else
+            {
+                AudioSourceLeft.Stop();
+                AudioSourceRight.Stop();
+            }
+            
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void ToggleStationInteractability(bool _active)
+        {
+            foreach (Collider _col in Collider)
+            {
+                if (_col == null) { Debug.LogWarning("Collider missing in collider list at" + gameObject.name); continue; }
+                _col.enabled = _active;
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+
+}
