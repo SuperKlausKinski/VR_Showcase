d62c70307a8e9bd2a2d2dabfeeb296f0fb71a031
Pepe <peerdraeger@googlemail.com>
2017-11-07 18:53:58 +0100

    more sound fx, cleaned up some things

---
+++ Assets/AnimationStation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace VRControlls.Demo
+{
+    public class AnimationStation : MonoBehaviour
+    {
+        public Collider[] Collider;
+
+        //-------------------------------------------------------------------------------------------------------------
+        public void ToggleStationInteractability(bool _active)
+        {
+            foreach (Collider _col in Collider)
+            {
+                if (_col == null) { Debug.LogWarning("Collider missing in collider list at" + gameObject.name); continue; }
+                _col.enabled = _active;
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+
+}
---
+++ Assets/RotateObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace VRControlls.Animation
+{
+    public class RotateObject : MonoBehaviour
+    {
+
+        private Tween m_RotationTween;
+        //-------------------------------------------------------------------------------------------------------------
+        public void RotateObjectAround(int _direction)
+        {
+            if (m_RotationTween == null)
+            {
+                m_RotationTween = transform.DOLocalRotate((Vector3.up * 180f)*_direction, 3.0f).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+            }
+            else
+            {
+                m_RotationTween.Play();
+            }
+        }
+        //-------------------------------------------------------------------------------------------------------------
+        public void StopObjectRotation()
+        {
+            Debug.Log("STop tween");
+            if (m_RotationTween != null)
+            {
+                m_RotationTween.Pause();
+            }
+
+        }
+        //-------------------------------------------------------------------------------------------------------------
+    }
+
+}
